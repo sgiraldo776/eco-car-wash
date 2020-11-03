96829be3f389f5912f9d3eed131e9793c62c5b54
sam-oro <samiroroz@gmail.com>
2020-11-03 09:00:55 -0500

    login no funcional

---
+++ Cliente/login/login.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ingresar</title>
+</head>
+
+<body>
+    <form action="">
+        <label for="">Correo</label>
+        <input type="text">
+
+        <label for="">Contraseña</label>
+        <input type="password">
+    </form>
+</body>
+
+</html>
