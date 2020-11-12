3aedb65c99258917eca24ff5987815a910a7e41b
Juangarzon11 <juangarzon07@gmail.com>
2020-11-12 11:35:55 -0500

    Restar Insumos

--- Admin/insumos/restar_insumo.php
+++ Admin/insumos/restar_insumo.php
@@ -5,17 +5,25 @@ require  "../../conexion.php";
 $Id_Insumo= $_GET['Id_Insumo'];
 $cantidad= $_POST['cantidad'];
 
-$cantidadactual = $conn ->query("SELECT Cantidad FROM tblinsumo_repuesto WHERE Id_Insumo='$Id_Insumo'");
+$sel = $conn -> query("SELECT * FROM tblinsumo_repuesto WHERE Id_Insumo='$Id_Insumo'");
 
-$cf=($cantidadactual)-($cantidad);
+while ($row=$sel->fetch_array()){
+    echo $row[1];
+    $cant=$row[1];
+    $valu=$row[3];
+}
 
-$sql = $conn ->query("UPDATE tblinsumo_repuesto SET Cantidad='$cf' WHERE Id_Insumo='$Id_Insumo'");
+$cf=($cant)-($cantidad);
+
+$valtotal=$cf*$valu;
+
+$sql = $conn ->query("UPDATE tblinsumo_repuesto SET Cantidad='$cf', Vlr_Total='$valtotal' WHERE Id_Insumo='$Id_Insumo'");
 
 if ($sql) {
-    //echo "<script> 	location.href='form_insumo.php'; </script>";
+    echo "<script> 	location.href='form_insumo.php'; </script>";
 }else{
-    //echo "<script> 	location.href='form_insumo.php'; </script>";
-    echo "<script> 	alert('pailas socio') </script>";
+    echo "<script> 	alert('ERROR') </script>";
+    echo "<script> 	location.href='form_insumo.php'; </script>";
 }
 
 
