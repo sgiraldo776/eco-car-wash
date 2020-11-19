3840f1ec47f46a63a474efe14aded026c31ed5ae
sam-oro <samiroroz@gmail.com>
2020-11-19 15:51:55 -0500

    conexion

--- conexion.php
+++ conexion.php
@@ -1,16 +1,12 @@
 <?php
 
     //Los otros jaja
-//$conn = new mysqli('localhost','root','','car_wash');
+// $conn = new mysqli('localhost','root','','car_wash');
 // $URL = "http://localhost/eco-car-wash/";
+
 
     //Samir
 $conn = new mysqli('localhost','root','','car_wash','33065');
 $URL = "http://localhost:8080/eco-car-wash/";
 
-<<<<<<< HEAD
-=======
-//$URL = "http://localhost/eco-car-wash/";
-$URL = "http://localhost/GIT/eco-car-wash/";
->>>>>>> 8bd545153fbdecf97a18cd436f6cb1380c84fb92
 ?>
